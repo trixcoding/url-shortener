89a6d54ca42a17fc341daced6774056f2c667039
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-03 23:51:49 +0330

    Update LiveClicksTable.tsx

--- myapp/app/dashboard/LiveClicksTable.tsx
+++ myapp/app/dashboard/LiveClicksTable.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { LinkWithClicks } from '../types/link';
+import { LinkWithClicks } from '../types/links';
 
 export default function LiveClicksTable({
   initialLinks,
