fca1de17db7af8cdf6a2d5b10ed3df567fa00f68
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-03 23:48:42 +0330

    Update page.tsx

--- myapp/app/page.tsx
+++ myapp/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const res = await fetch('/api/links', {
+      const res = await fetch('/api/test/links', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ originalUrl: url }),
