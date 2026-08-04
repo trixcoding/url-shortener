a4bdca7dc215f80ce9492eabe29679d2ee958c1f
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-04 17:42:07 +0330

    Update queue.ts

--- myapp/app/lib/queue.ts
+++ myapp/app/lib/queue.ts
@@ -10,6 +10,9 @@ export function getQueue(): Queue {
       maxRetriesPerRequest: null,
       tls: {},
     });
+    connection.on('error', (err) => {
+      console.error('خطای اتصال به Redis:', err.message);
+    });
   }
   if (!cleanupQueue) {
     cleanupQueue = new Queue('cleanup', { connection });
