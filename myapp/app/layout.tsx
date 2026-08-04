ba71e7012e74924cab4415624baaadc72a3d3962
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-05 02:42:46 +0330

    Update layout.tsx

--- myapp/app/layout.tsx
+++ myapp/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,9 +28,25 @@ export default function RootLayout({
       lang="fa" dir="rtl"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}
-<Toaster position="top-center" />
-</body>
+      <body className="min-h-full flex flex-col">
+        <div className="flex-1">{children}</div>
+
+        <footer className="text-xs text-blue-600 text-center py-4 border-t border-blue-200">
+          ساخته شده توسط <a
+            href="https://mhmdnsr.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-medium text-blue-700 hover:text-blue-900 underline"
+          >MhmdNsr</a> | <a
+            href="https://t.me/mhmdnsrr"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-700 hover:text-blue-900 underline"
+          >تلگرام</a>
+        </footer>
+
+        <Toaster position="top-center" />
+      </body>
     </html>
   );
 }
