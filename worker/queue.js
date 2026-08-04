ae9adbbd1349181b0b16808b8712d379c9225537
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-04 16:35:17 +0330

    Update queue.js

--- worker/queue.js
+++ worker/queue.js
@@ -1,7 +1,7 @@
 import { Queue } from 'bullmq';
 import IORedis from 'ioredis';
 
-const connection = new IORedis(process.env.REDIS_URL, {
+const connection = new IORedis(process.env.REDISS_URL, {
   maxRetriesPerRequest: null,
   tls: {},
 });
