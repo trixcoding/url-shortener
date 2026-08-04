67a93f653d8f673ed905a34a5c689f209bc307ce
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-05 02:30:56 +0330

    Update layout.tsx

--- myapp/app/layout.tsx
+++ myapp/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from 'react-hot-toast';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
